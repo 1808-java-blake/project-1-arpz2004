00bfbc538b800ddfd3e12f8a6f5d68714d942e0d
Adrien Lindner <lindner.54@osu.edu>
2018-09-05 14:24:32 -0400

    Fixed env variables

--- server/src/index.ts
+++ server/src/index.ts
@@ -39,7 +39,7 @@ app.use(bodyParser.json());
 app.use((req, resp, next) => {
   (process.env.REIMBURSEMENT_API_STAGE === 'prod')
     ? resp.header('Access-Control-Allow-Origin', process.env.DEMO_APP_URL)
-    : resp.header('Access-Control-Allow-Origin', `http://localhost:9001`);
+    : resp.header('Access-Control-Allow-Origin', `http://localhost:3000`);
   resp.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   resp.header('Access-Control-Allow-Credentials', 'true');
   next();
--- server/src/util/connection-util.ts
+++ server/src/util/connection-util.ts
@@ -2,9 +2,9 @@ import { Pool } from 'pg';
 
 export const connectionPool = new Pool({
   database: 'postgres',
-  host: process.env["1808_MOVIE_DB_HOST"],
+  host: process.env["MOVIE_DB_HOST"],
   max: 2,
-  password: process.env["1808_MOVIE_DB_PASSWORD"],
+  password: process.env["MOVIE_DB_PASSWORD"],
   port: 5432,
-  user: process.env["1808_MOVIE_DB_USERNAME"]
+  user: process.env["MOVIE_DB_USERNAME"]
 })
