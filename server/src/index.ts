1e8eb0013fab2d2ac256c1d1ec0315035933b26a
Adrien Lindner <lindner.54@osu.edu>
2018-09-05 15:04:25 -0400

    Fixed CORS header

--- server/src/index.ts
+++ server/src/index.ts
@@ -38,7 +38,7 @@ app.use(bodyParser.json());
 
 app.use((req, resp, next) => {
   (process.env.REIMBURSEMENT_API_STAGE === 'prod')
-    ? resp.header('Access-Control-Allow-Origin', process.env.DEMO_APP_URL)
+    ? resp.header('Access-Control-Allow-Origin', process.env.REIMBURSEMENT_APP_URL)
     : resp.header('Access-Control-Allow-Origin', `http://localhost:3000`);
   resp.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   resp.header('Access-Control-Allow-Credentials', 'true');
