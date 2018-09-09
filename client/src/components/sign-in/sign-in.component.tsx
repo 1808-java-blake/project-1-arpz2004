75b81db18f93d5880271e4d6d98875960a8085f9
Adrien Lindner <lindner.54@osu.edu>
2018-09-08 23:39:53 -0400

    Placed signin form into a card

--- client/src/components/sign-in/sign-in.component.tsx
+++ client/src/components/sign-in/sign-in.component.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
 import { RouteComponentProps } from 'react-router';
-import { Button } from 'reactstrap';
+import { Button, CardBody, CardHeader } from 'reactstrap';
+import Card from 'reactstrap/lib/Card';
 import Col from 'reactstrap/lib/Col';
-import Container from 'reactstrap/lib/Container';
 import Form from 'reactstrap/lib/Form';
 import FormGroup from 'reactstrap/lib/FormGroup';
 import Input from 'reactstrap/lib/Input';
@@ -35,41 +35,41 @@ class SignInComponent extends React.Component<IProps, {}> {
   public render() {
     const { errorMessage, credentials } = this.props;
     return (
-      <Container className="col-md-3 col-md-offset-3 border border-primary rounded bg-light" id="login-form">
-        <div className="form-padding">
-          <h5>Sign In to Start a Reimbursement Request</h5>
-          <Form onSubmit={(e: React.FormEvent<HTMLFormElement>) => this.props.login(e, this.props.credentials)}>
-            <Col>
-              <FormGroup>
-                <Input
-                  onChange={(e: any) => this.props.updateUsername(e.target.value)}
-                  value={credentials.username}
-                  type="text"
-                  id="inputUsername"
-                  className="form-control"
-                  placeholder="Username"
-                  required
-                />
-              </FormGroup>
-            </Col>
-            <Col>
-              <FormGroup>
-                <Input
-                  onChange={(e: any) => this.props.updatePassword(e.target.value)}
-                  value={credentials.password}
-                  type="password"
-                  id="inputPassword"
-                  className="form-control"
-                  placeholder="Password"
-                  required
-                />
-              </FormGroup>
-            </Col>
-            <Button color="primary" className="btn btn-lg btn-block" type="submit">Sign In</Button>
-          </Form>
-          {errorMessage && <p id="error-message">{errorMessage}</p>}
-        </div>
-      </Container>
+        <Card>
+          <CardHeader className="text-white bg-dark">Sign In to Start a Reimbursement Request</CardHeader>
+          <CardBody className="bg-light">
+            <Form onSubmit={(e: React.FormEvent<HTMLFormElement>) => this.props.login(e, this.props.credentials)}>
+              <Col>
+                <FormGroup>
+                  <Input
+                    onChange={(e: any) => this.props.updateUsername(e.target.value)}
+                    value={credentials.username}
+                    type="text"
+                    id="inputUsername"
+                    className="form-control"
+                    placeholder="Username"
+                    required
+                  />
+                </FormGroup>
+              </Col>
+              <Col>
+                <FormGroup>
+                  <Input
+                    onChange={(e: any) => this.props.updatePassword(e.target.value)}
+                    value={credentials.password}
+                    type="password"
+                    id="inputPassword"
+                    className="form-control"
+                    placeholder="Password"
+                    required
+                  />
+                </FormGroup>
+              </Col>
+              <Button color="primary" className="btn btn-lg btn-block" type="submit">Sign In</Button>
+            </Form>
+            {errorMessage && <p id="error-message">{errorMessage}</p>}
+          </CardBody>
+        </Card>
     );
   }
 }
